833497d5d360cca741217c0cd0f5190e9ccf20dc
tvattima <github@vattima.com>
2019-05-15 22:09:15 -0400

    Enhancing SaleItemDescriptionBuilder

--- src/main/java/com/vattima/bricklink/inventory/service/SaleItemDescriptionBuilder.java
+++ src/main/java/com/vattima/bricklink/inventory/service/SaleItemDescriptionBuilder.java
@@ -2,13 +2,69 @@ package com.vattima.bricklink.inventory.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.bricklink.data.lego.dto.BricklinkInventory;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class SaleItemDescriptionBuilder {
-    public String buildDescription() {
-        return "";
+    public String buildDescription(BricklinkInventory bricklinkInventory) {
+        ConditionDecoder conditionDecoder = new ConditionDecoder();
+        StringBuilder description = new StringBuilder();
+
+        // Extra Description
+        Optional.ofNullable(bricklinkInventory.getExtraDescription()).ifPresent(description::append);
+
+        // Box
+        conditionDecoder.decode(bricklinkInventory.getBoxConditionCode()).ifPresent(c -> {
+            append(description, String.format("Box: %s", c));
+        });
+
+        // Sealed
+        if (bricklinkInventory.isSealed()) {
+            append(description, "Sealed");
+        }
+
+        // Instructions
+        conditionDecoder.decode(bricklinkInventory.getInstructionsConditionCode()).ifPresent(c -> {
+            append(description, String.format("Instructions: %s", c));
+        });
+
+        log.info("Description [{}]", description.toString());
+        return description.toString();
+    }
+
+    private StringBuilder append(StringBuilder stringBuilder, String s) {
+        if (stringBuilder.length() > 0) {
+            stringBuilder.append(", ");
+        }
+        return stringBuilder.append(s);
+    }
+
+    static class ConditionDecoder {
+        private Map<String, String> conditionDescriptions = new HashMap<>();
+
+        public ConditionDecoder() {
+            conditionDescriptions.put("M", "Mint");
+            conditionDescriptions.put("E", "Excellent");
+            conditionDescriptions.put("VG", "Very Good");
+            conditionDescriptions.put("G", "Good");
+            conditionDescriptions.put("P", "Poor");
+            conditionDescriptions.put("NA", "Not Applicable");
+            conditionDescriptions.put("F", "Fair");
+            conditionDescriptions.put("MS", "Missing");
+            conditionDescriptions.put("CC", "Color Copy");
+            conditionDescriptions.put("BW", "Black & White Copy");
+            conditionDescriptions.put("SL", "Sealed");
+        }
+
+        public Optional<String> decode(String conditionCode) {
+            return Optional.ofNullable(conditionDescriptions.get(conditionCode));
+        }
     }
 }
--- src/test/java/com/vattima/bricklink/inventory/service/SaleItemDescriptionBuilderTest.java
+++ src/test/java/com/vattima/bricklink/inventory/service/SaleItemDescriptionBuilderTest.java
@@ -1,7 +1,11 @@
 package com.vattima.bricklink.inventory.service;
 
+import net.bricklink.data.lego.dto.BricklinkInventory;
 import org.junit.Test;
 
+import java.util.Optional;
+
+import static com.vattima.bricklink.inventory.service.SaleItemDescriptionBuilder.ConditionDecoder;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SaleItemDescriptionBuilderTest {
@@ -9,7 +13,99 @@ public class SaleItemDescriptionBuilderTest {
     @Test
     public void buildDescription_returnsNonNull() {
         SaleItemDescriptionBuilder saleItemDescriptionBuilder = new SaleItemDescriptionBuilder();
-        String description = saleItemDescriptionBuilder.buildDescription();
-        assertThat(description).isNotNull();
+
+        BricklinkInventory bricklinkInventory = new BricklinkInventory();
+        String description = saleItemDescriptionBuilder.buildDescription(bricklinkInventory);
+
+        assertThat(description).isEmpty();
+    }
+
+    @Test
+    public void buildDescription_withBoxConditionCode_containsBoxDescription() {
+        SaleItemDescriptionBuilder saleItemDescriptionBuilder = new SaleItemDescriptionBuilder();
+
+        BricklinkInventory bricklinkInventory = new BricklinkInventory();
+        bricklinkInventory.setBoxConditionCode("M");
+        String description = saleItemDescriptionBuilder.buildDescription(bricklinkInventory);
+        assertThat(description).contains("Box: Mint");
+
+        bricklinkInventory.setBoxConditionCode("E");
+        description = saleItemDescriptionBuilder.buildDescription(bricklinkInventory);
+        assertThat(description).contains("Box: Excellent");
+    }
+
+    @Test
+    public void buildDescription_withSealedAndBoxConditionCode_containsBoxDescription() {
+        SaleItemDescriptionBuilder saleItemDescriptionBuilder = new SaleItemDescriptionBuilder();
+
+        BricklinkInventory bricklinkInventory = new BricklinkInventory();
+        bricklinkInventory.setSealed(true);
+        bricklinkInventory.setBoxConditionCode("E");
+        String description = saleItemDescriptionBuilder.buildDescription(bricklinkInventory);
+        assertThat(description).contains("Box: Excellent");
+        assertThat(description).contains("Sealed");
+    }
+
+    @Test
+    public void buildDescription_withExtraDescription() {
+        SaleItemDescriptionBuilder saleItemDescriptionBuilder = new SaleItemDescriptionBuilder();
+
+        BricklinkInventory bricklinkInventory = new BricklinkInventory();
+        bricklinkInventory.setExtraDescription("This is an awesome set!");
+        bricklinkInventory.setSealed(true);
+        bricklinkInventory.setBoxConditionCode("E");
+        bricklinkInventory.setInstructionsConditionCode("M");
+        String description = saleItemDescriptionBuilder.buildDescription(bricklinkInventory);
+        assertThat(description).contains("This is an awesome set!");
+        assertThat(description).contains("Box: Excellent");
+        assertThat(description).contains("Sealed");
+    }
+
+    @Test
+    public void buildDescription_withInstructionsConditionCode_containsInstructionsDescription() {
+        SaleItemDescriptionBuilder saleItemDescriptionBuilder = new SaleItemDescriptionBuilder();
+
+        BricklinkInventory bricklinkInventory = new BricklinkInventory();
+        bricklinkInventory.setInstructionsConditionCode("M");
+        String description = saleItemDescriptionBuilder.buildDescription(bricklinkInventory);
+        assertThat(description).contains("Instructions: Mint");
+
+        bricklinkInventory.setInstructionsConditionCode("MS");
+        description = saleItemDescriptionBuilder.buildDescription(bricklinkInventory);
+        assertThat(description).contains("Instructions: Missing");
+
+        bricklinkInventory.setInstructionsConditionCode("CC");
+        description = saleItemDescriptionBuilder.buildDescription(bricklinkInventory);
+        assertThat(description).contains("Instructions: Color Copy");
+
+        bricklinkInventory.setInstructionsConditionCode("BW");
+        description = saleItemDescriptionBuilder.buildDescription(bricklinkInventory);
+        assertThat(description).contains("Instructions: Black & White Copy");
+    }
+
+    @Test
+    public void buildDescription_withBoxAndInstructionsConditionCode_containsBoxAndInstructionsDescription() {
+        SaleItemDescriptionBuilder saleItemDescriptionBuilder = new SaleItemDescriptionBuilder();
+
+        BricklinkInventory bricklinkInventory = new BricklinkInventory();
+        bricklinkInventory.setBoxConditionCode("E");
+        bricklinkInventory.setInstructionsConditionCode("VG");
+        String description = saleItemDescriptionBuilder.buildDescription(bricklinkInventory);
+        assertThat(description).contains("Box: Excellent");
+        assertThat(description).contains("Instructions: Very Good");
+    }
+
+    @Test
+    public void conditionDecoder_returnsCondition_forKnownValue() {
+        ConditionDecoder conditionDecoder = new ConditionDecoder();
+        Optional<String> condition = conditionDecoder.decode("M");
+        assertThat(condition).isPresent().get().isEqualTo("Mint");
+    }
+
+    @Test
+    public void conditionDecoder_returnsNotPresent_forUnknownValue() {
+        ConditionDecoder conditionDecoder = new ConditionDecoder();
+        Optional<String> condition = conditionDecoder.decode("XX");
+        assertThat(condition).isNotPresent();
     }
 }
