4b00ab12a7d30c32c33f3bab539a7a70b595f65f
tvattima <github@vattima.com>
2019-06-16 21:40:10 -0400

    Updating PriceCalculatorService Adding .gitignore to avoid committing IntelliJ iml files

--- src/main/java/com/vattima/bricklink/inventory/service/PriceCalculatorService.java
+++ src/main/java/com/vattima/bricklink/inventory/service/PriceCalculatorService.java
@@ -1,7 +1,101 @@
 package com.vattima.bricklink.inventory.service;
 
+import com.bricklink.api.rest.client.BricklinkRestClient;
+import com.vattima.lego.inventory.pricing.PriceNotCalculableException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.bricklink.data.lego.dao.BricklinkInventoryDao;
+import net.bricklink.data.lego.dao.BricklinkSaleItemDao;
+import net.bricklink.data.lego.dto.BricklinkInventory;
+import net.bricklink.data.lego.dto.BricklinkSaleItem;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
 public class PriceCalculatorService {
-    public double calculatePrice(double[] prices) {
-        return 0.0d;
+    private final BricklinkSaleItemDao bricklinkSaleItemDao;
+    private final BricklinkInventoryDao bricklinkInventoryDao;
+    private final BricklinkRestClient bricklinkRestClient;
+
+    public double calculatePrice(final BricklinkInventory bricklinkInventory) {
+        double price = 0.0d;
+        double newPrice = 0.0d;
+        double usedPrice = 0.0d;
+        double[] newPrices = getNewPrices(bricklinkInventory.getBlItemId(), bricklinkInventory.getCompleteness());
+        double[] usedPrices = getUsedPrices(bricklinkInventory.getBlItemId(), bricklinkInventory.getCompleteness());
+
+        int newPricesCount = newPrices.length;
+        int usedPricesCount = usedPrices.length;
+        if ("N".equals(bricklinkInventory.getNewOrUsed())) {
+            if (newPricesCount > 2) {
+                newPrice = calculatePrice(bricklinkInventory, newPrices);
+            } else if (newPricesCount > 1) {
+                newPrice = calculatePriceUsingTwoPrices(bricklinkInventory, newPrices);
+            } else if (newPricesCount > 0) {
+                newPrice = calculatePriceUsingOnePrice(bricklinkInventory, newPrices);
+            } else {
+                throw new PriceNotCalculableException(bricklinkInventory, "There are no prices with which to calculate a price");
+            }
+            price = newPrice;
+        } else {
+            if (usedPricesCount > 2) {
+                usedPrice = calculatePrice(bricklinkInventory, usedPrices);
+            } else if (usedPricesCount > 1) {
+                usedPrice = calculatePriceUsingTwoPrices(bricklinkInventory, usedPrices);
+            } else if (usedPricesCount > 0) {
+                usedPrice = calculatePriceUsingOnePrice(bricklinkInventory, usedPrices);
+            } else {
+                throw new PriceNotCalculableException(bricklinkInventory, "There are no prices with which to calculate a price");
+            }
+            price = usedPrice;
+        }
+        return price;
+    }
+
+    public double[] getNewPrices(final Long blItemId, final String completeness) {
+        return getPrices(blItemId, "N", completeness);
+    }
+
+    public double[] getUsedPrices(final Long blItemId, final String completeness) {
+        return getPrices(blItemId, "U", completeness);
+    }
+
+    public double[] getPrices(final Long blItemId, final String newOrUsed, final String completeness) {
+        return bricklinkSaleItemDao.getPricesForItem(blItemId, newOrUsed, completeness)
+                                   .stream()
+                                   .filter(bi -> bi.getStatus().equals("C"))
+                                   .sorted(Comparator.comparing(BricklinkSaleItem::getUnitPrice))
+                                   .mapToDouble(BricklinkSaleItem::getUnitPrice)
+                                   .toArray();
+    }
+
+    public double calculatePriceUsingTwoPrices(final BricklinkInventory bricklinkInventory, double[] prices) {
+        double lowPrice = prices[0];
+        double highPrice = prices[1];
+        return lowPrice + (highPrice - lowPrice) * 0.75d;
+    }
+
+    public double calculatePriceUsingOnePrice(final BricklinkInventory bricklinkInventory, double[] prices) {
+        double price = prices[0];
+        return price * 0.85d;
+    }
+
+    public double calculatePrice(final BricklinkInventory bricklinkInventory, double[] prices) {
+        DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics();
+        if ((prices[prices.length - 1] / prices[prices.length - 2]) > 3) {
+            throw new PriceNotCalculableException(bricklinkInventory, "["+bricklinkInventory.getNewOrUsed()+" "+bricklinkInventory.getCompleteness()+" "+bricklinkInventory.getBlItemNo()+" "+bricklinkInventory.getBlItemId()+"] Delta between last two prices is too high [" + prices[prices.length - 1] + "], [" + prices[prices.length - 2] + "]");
+        }
+        Arrays.stream(prices)
+              .forEach(p -> {
+                  descriptiveStatistics.addValue(p);
+              });
+        double mean = descriptiveStatistics.getMean();
+        double standardDeviation = descriptiveStatistics.getStandardDeviation();
+        return mean + standardDeviation;
     }
 }
--- src/test/java/com/vattima/bricklink/inventory/service/PriceCalculatorServiceTest.java
+++ src/test/java/com/vattima/bricklink/inventory/service/PriceCalculatorServiceTest.java
@@ -4,15 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.DoubleStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class PriceCalculatorServiceTest {
     @Test
     public void calculatePrice() {
-        PriceCalculatorService priceCalculatorService = new PriceCalculatorService();
-        double actualPrice = priceCalculatorService.calculatePrice(new double[]{});
-        assertThat(actualPrice).isEqualTo(0.0d);
+        PriceCalculatorService priceCalculatorService = new PriceCalculatorService(null, null, null);
+        double actualPrice = priceCalculatorService.calculatePrice(null, new double[]{});
+        assertThat(actualPrice).isNaN();
 
         double[] prices = new double[]{5.67d, 9.45d, 2.17d, 11.50d, 8.34d, 6.15d, 10.01d, 7.71d, 5.92d, 9.51d};
         DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics();
@@ -46,4 +50,21 @@ public class PriceCalculatorServiceTest {
         log.info("standard deviation [{}]", standardDeviation2);
     }
 
+    @Test
+    public void calculatePrice_usingService() {
+        double[] prices = new double[]{5.67d, 9.45d, 15.23d, 11.50d, 28.34d, 36.15d, 88.01d, 17.71d, 15.92d, 29.51d};
+        prices = Arrays.stream(prices).sorted().toArray();
+        for (double d : prices) {
+            log.info("[{}]", d);
+        }
+        PriceCalculatorService priceCalculatorService = new PriceCalculatorService(null, null, null);
+        double price = priceCalculatorService.calculatePrice(null, prices);
+        log.info("Price: [{}]", price);
+
+
+        prices = new Random().doubles(287314).map(d -> d * 100).sorted().toArray();
+        priceCalculatorService = new PriceCalculatorService(null, null, null);
+        price = priceCalculatorService.calculatePrice(null, prices);
+        log.info("Price: [{}]", price);
+    }
 }
