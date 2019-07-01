e55183b132f8d8e4eaa062c10a38e6897309c31c
tvattima <github@vattima.com>
2019-06-30 22:56:01 -0400

    * Added box and instruction condition to price calculation   - PriceCalculatorService   - PriceCalculatorServiceTest
    * Added setPrice method
      - BricklinkInventoryDao
      - BricklinkInventoryMapper
    
    * Invokes setPrice
      - InventoryCommand

--- src/test/java/com/vattima/bricklink/inventory/service/PriceCalculatorServiceTest.java
+++ src/test/java/com/vattima/bricklink/inventory/service/PriceCalculatorServiceTest.java
@@ -1,6 +1,8 @@
 package com.vattima.bricklink.inventory.service;
 
+import com.vattima.lego.inventory.pricing.PriceNotCalculableException;
 import lombok.extern.slf4j.Slf4j;
+import net.bricklink.data.lego.dto.BricklinkInventory;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.junit.Test;
 
@@ -9,14 +11,19 @@ import java.util.Random;
 import java.util.stream.DoubleStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 public class PriceCalculatorServiceTest {
     @Test
     public void calculatePrice() {
-        PriceCalculatorService priceCalculatorService = new PriceCalculatorService(null, null, null);
-        double actualPrice = priceCalculatorService.calculatePrice(null, new double[]{});
-        assertThat(actualPrice).isNaN();
+        PriceCalculatorService priceCalculatorService = new PriceCalculatorService(null);
+
+        assertThatThrownBy(() -> {
+            priceCalculatorService.calculatePrice(null, new double[]{});
+        }).isInstanceOf(PriceNotCalculableException.class)
+          .hasMessageContaining("There are no prices");
 
         double[] prices = new double[]{5.67d, 9.45d, 2.17d, 11.50d, 8.34d, 6.15d, 10.01d, 7.71d, 5.92d, 9.51d};
         DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics();
@@ -57,14 +64,45 @@ public class PriceCalculatorServiceTest {
         for (double d : prices) {
             log.info("[{}]", d);
         }
-        PriceCalculatorService priceCalculatorService = new PriceCalculatorService(null, null, null);
-        double price = priceCalculatorService.calculatePrice(null, prices);
-        log.info("Price: [{}]", price);
+        PriceCalculatorService priceCalculatorService = new PriceCalculatorService(null);
+        log.info("Price: [{}]", priceCalculatorService.calculatePrice(build("SL", "M"), prices));
+        log.info("Price: [{}]", priceCalculatorService.calculatePrice(build("M", "M"), prices));
+        log.info("Price: [{}]", priceCalculatorService.calculatePrice(build("E", "E"), prices));
+        log.info("Price: [{}]", priceCalculatorService.calculatePrice(build("VG", "VG"), prices));
+        log.info("Price: [{}]", priceCalculatorService.calculatePrice(build("G", "G"), prices));
+        log.info("Price: [{}]", priceCalculatorService.calculatePrice(build("F", "F"), prices));
+        log.info("Price: [{}]", priceCalculatorService.calculatePrice(build("P", "P"), prices));
+        log.info("Price: [{}]", priceCalculatorService.calculatePrice(build("MS", "MS"), prices));
 
 
         prices = new Random().doubles(287314).map(d -> d * 100).sorted().toArray();
-        priceCalculatorService = new PriceCalculatorService(null, null, null);
-        price = priceCalculatorService.calculatePrice(null, prices);
+        priceCalculatorService = new PriceCalculatorService(null);
+        double price = priceCalculatorService.calculatePrice(build("E", "E"), prices);
         log.info("Price: [{}]", price);
+    }
+
+    @Test
+    public void getPriceAdjustment_variousCombinations() {
+        PriceCalculatorService priceCalculatorService = new PriceCalculatorService(null);
+        double priceAdjustment = priceCalculatorService.getPriceAdjustment(build("SL", "M"));
+        assertThat(priceAdjustment).isEqualTo(1.25d);
+    }
+
+    private BricklinkInventory build(final String boxConditionCode, final String instructionsConditionCode) {
+//        SL	Sealed
+//        M	Mint
+//        E	Excellent
+//        VG	Very Good
+//        G	Good
+//        P	Poor
+//        NA	Not Applicable
+//        F	Fair
+//        MS	Missing
+//        CC	Color Copy
+//        BW	Black & White Copy
+        BricklinkInventory bricklinkInventory = new BricklinkInventory();
+        bricklinkInventory.setBoxConditionCode(boxConditionCode);
+        bricklinkInventory.setInstructionsConditionCode(instructionsConditionCode);
+        return bricklinkInventory;
     }
 }
